710aa675000ac62c3220d2c4b6e6b6ffe1c1d33b
Brandon Pliska <brandon.pliska@gmail.com>
2023-08-14 15:49:26 -0700

    frontend stubbed

--- silk/apps/findings/src/app/app.controller.spec.ts
+++ silk/apps/findings/src/app/app.controller.spec.ts
@@ -14,7 +14,7 @@ describe('AppController', () => {
   })
 
   describe('getData', () => {
-    it('should return "Hello API"', () => {
+    it('should return "Hello from Brandon!"', () => {
       const appController = app.get<AppController>(AppController)
       expect(appController.getData()).toEqual({
         message: 'Hello from Brandon!',
