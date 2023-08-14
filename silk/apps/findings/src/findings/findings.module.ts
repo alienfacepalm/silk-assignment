fcb24970efac44ddccb36cf683c67ce189acff15
Brandon Pliska <brandon.pliska@gmail.com>
2023-08-14 15:40:51 -0700

    fix tests

--- silk/apps/findings/src/findings/findings.module.ts
+++ silk/apps/findings/src/findings/findings.module.ts
@@ -1,6 +1,6 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
 
-import { FindingsService } from './findings.service';
+import { FindingsService } from './findings.service'
 
 @Module({
   imports: [],
