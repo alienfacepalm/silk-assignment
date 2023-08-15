ebf997b43a925052cdce6a498bd20e958e181747
Brandon Pliska <brandon.pliska@gmail.com>
2023-08-15 12:27:09 -0700

    map statuses

--- silk/apps/dashboard/src/app/components/table/status.tsx
+++ silk/apps/dashboard/src/app/components/table/status.tsx
@@ -3,19 +3,38 @@ import { upperCase, startCase } from 'lodash'
 
 import './table.css'
 
-export const statusColorMap: Record<string, string> = {
+const statusColorMap: Record<string, string> = {
   pending: '#e6cc00',
-  in_progress: '#378805',
+  in_progress: '#149414',
   halted: '#ff6600',
   cancelled: '#ff0000',
   complete: '#378bf1',
 }
 
-export const Status: React.FC<{ value: string }> = ({ value }) => (
-  <>
-    <div className="status" style={{ backgroundColor: statusColorMap[value] }}>
-      {upperCase(startCase(value))}
-    </div>
-    <div className="progress"></div>
-  </>
-)
+const statusProgressMap: Record<string, number> = {
+  pending: 0,
+  in_progress: 50,
+  halted: 50,
+  cancelled: 100,
+  complete: 100,
+}
+
+export const Status: React.FC<{ value: string }> = ({ value }) => {
+  const color = statusColorMap[value]
+  return (
+    <>
+      <div
+        className="status"
+        style={{ backgroundColor: statusColorMap[value] }}
+      >
+        {upperCase(startCase(value))}
+      </div>
+      <div
+        className="progress"
+        style={{
+          background: `linear-gradient(to right, ${color} ${statusProgressMap[value]}%, transparent 50%)`,
+        }}
+      ></div>
+    </>
+  )
+}
