30f89ca7b7fbbc0579b0de55a2707953dda33af2
Brandon Pliska <brandon.pliska@gmail.com>
2023-08-14 21:08:34 -0700

    install nx

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 cd silk
+
+
+if ! command -v nx &> /dev/null
+then
+    echo "nx command not found, installing with npm i -g nx"
+    npm i -g nx
+fi
 nx run-many --target=serve --projects=findings,dashboard
+
