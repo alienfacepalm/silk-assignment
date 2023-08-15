0767af8c598b7ece9dd6a46af13d10872146dbeb
Brandon Pliska <brandon.pliska@gmail.com>
2023-08-15 15:36:04 -0700

    simplify chart

--- silk/apps/dashboard/src/app/components/pie-chart/pie-chart.tsx
+++ silk/apps/dashboard/src/app/components/pie-chart/pie-chart.tsx
@@ -1,16 +1,9 @@
 import React from 'react'
-import { upperCase, startCase } from 'lodash'
-import {
-  PieChart as Chart,
-  pieChartDefaultProps,
-} from 'react-minimal-pie-chart'
+import { PieChart as Chart } from 'react-minimal-pie-chart'
 import { Data } from 'react-minimal-pie-chart/types/commonTypes'
 
 const shiftSize: number = 6
 export const PieChart: React.FC<{ data: Data }> = ({ data }) => {
-  const [selected, setSelected] = React.useState<number>(0)
-  const [hovered, setHovered] = React.useState<number | undefined>(undefined)
-
   return (
     <Chart
       data={data}
@@ -19,8 +12,6 @@ export const PieChart: React.FC<{ data: Data }> = ({ data }) => {
       labelStyle={{
         fontSize: '0.65em',
       }}
-      radius={pieChartDefaultProps.radius - shiftSize}
-      segmentsShift={(index) => (index === 0 ? shiftSize : 0.5)}
     />
   )
 }
