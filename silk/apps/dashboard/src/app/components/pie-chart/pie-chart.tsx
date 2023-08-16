1c31ae6bfab9abe534cfe4e58b7c069625c0fb5a
Brandon Pliska <brandon.pliska@gmail.com>
2023-08-15 22:34:56 -0700

    wip, custom round

--- silk/apps/dashboard/src/app/components/pie-chart/pie-chart.tsx
+++ silk/apps/dashboard/src/app/components/pie-chart/pie-chart.tsx
@@ -2,16 +2,14 @@ import React from 'react'
 import { PieChart as Chart } from 'react-minimal-pie-chart'
 import { Data } from 'react-minimal-pie-chart/types/commonTypes'
 
-let i = 0
 function round(value: number): number {
-  let output: number
-  if (i % 2 === 0) {
-    output = Math.floor(value)
+  const decimalPart = value - Math.floor(value)
+
+  if (decimalPart > 0.8) {
+    return Math.ceil(value)
   } else {
-    output = Math.ceil(value)
+    return Math.floor(value)
   }
-  console.log({ output })
-  return output
 }
 
 export const PieChart: React.FC<{ data: Data }> = ({ data }) => (
