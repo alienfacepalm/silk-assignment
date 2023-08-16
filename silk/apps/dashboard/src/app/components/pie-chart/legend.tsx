39c01b07d82fa0e0695230ff404bf6c27279d619
Brandon Pliska <brandon.pliska@gmail.com>
2023-08-15 23:35:57 -0700

    rm unnecessary data param

--- silk/apps/dashboard/src/app/components/pie-chart/legend.tsx
+++ silk/apps/dashboard/src/app/components/pie-chart/legend.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { severityColorMap } from '../../util'
 
-export const Legend: React.FC<{ data: any }> = ({ data }) => (
+export const Legend: React.FC = () => (
   <div className="flex justify-center mt-4">
     {Object.keys(severityColorMap).map((severity, index) => (
       <div key={index} className="flex items-center mr-4">
